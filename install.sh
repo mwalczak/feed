05d914b8385e332d79f1fffdc540974627c9f438
mwalczak <m.walczak@inis.pl>
2019-01-21 08:44:24 +0100

    custom js

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 cp src/settings.php.dist src/settings.php
 cp templates/include/product.twig.dist templates/include/product.twig
+cp templates/include/products.twig.dist templates/include/products.twig
