b5c24b402cf96bebf70ff2ab9cbfcd8103d23aac
mwalczak <m.walczak@inis.pl>
2019-01-21 16:21:00 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cp src/settings.php.dist src/settings.php
-cp templates/include/product.twig.dist templates/include/product.twig
-cp templates/include/products.twig.dist templates/include/products.twig
-cp templates/include/cart.twig.dist templates/include/cart.twig
-cp templates/include/registration.twig.dist templates/include/registration.twig
-cp templates/include/delivery.twig.dist templates/include/delivery.twig
-cp templates/include/payment.twig.dist templates/include/payment.twig
-cp templates/include/checkout.twig.dist templates/include/checkout.twig
+cp templates/script/product.twig.dist templates/script/product.twig
+cp templates/script/products.twig.dist templates/script/products.twig
+cp templates/script/cart.twig.dist templates/script/cart.twig
+cp templates/script/registration.twig.dist templates/script/registration.twig
+cp templates/script/delivery.twig.dist templates/script/delivery.twig
+cp templates/script/payment.twig.dist templates/script/payment.twig
+cp templates/script/checkout.twig.dist templates/script/checkout.twig
